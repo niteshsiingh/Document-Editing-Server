3d4561da981936285a6d777e8776e9495688d504
Nitesh Singh <nitesh.singh@matrice.ai>
2024-11-11 01:19:46 +0530

    added all files

--- src/config/db-init.go
+++ src/config/db-init.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
 
@@ -17,19 +16,22 @@ func InitializeDatabase() (*databases.Queries, error) {
 
 	var dsn string
 	if env == "prod" {
-		dsn = fmt.Sprintf("%s?sslmode=require&sslrootcert=/path/to/ca.crt", dbURL)
+		// dsn = fmt.Sprintf("%s?sslmode=require&sslrootcert=/path/to/ca.crt", dbURL)
+		dsn = dbURL
 	} else {
-		dbUser := os.Getenv("USER")
-		dbPass := os.Getenv("PASSWORD")
-		dbHost := os.Getenv("DB_HOST")
-		dbPort := os.Getenv("DB_PORT")
-		dbName := os.Getenv("DATABASE")
-		dsn = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPass, dbName)
+		// dbUser := os.Getenv("DB_USER")
+		// dbPass := os.Getenv("DB_PASSWORD")
+		// dbHost := os.Getenv("DB_HOST")
+		// dbPort := os.Getenv("DB_PORT")
+		// dbName := os.Getenv("DB_NAME")
+		dsn = dbURL
+		// dsn = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPass, dbName)
 	}
 
 	ctx := context.Background()
 	conn, err := pgxpool.New(ctx, dsn)
 	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
 		return nil, err
 	}
 	// defer conn.Close(ctx)
